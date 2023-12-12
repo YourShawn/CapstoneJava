e8da7f8cd2e1d3f1ca2b5607f8be51f539e27368
UmmeaymanMirza <ayman.kapadia@gmail.com>
2023-12-11 22:40:47 -0500

    Change in DoctorServiceImpl

--- src/main/java/com/capstone/healthcare/service/impl/DoctorsServiceImpl.java
+++ src/main/java/com/capstone/healthcare/service/impl/DoctorsServiceImpl.java
@@ -86,7 +86,7 @@ public class DoctorsServiceImpl implements DoctorsService {
 
     @Override
     public List<Object[]> findList(){
-        
+
         List<Object[]> listByQuery = doctorsJPA2.getDoctorsList();
         return listByQuery;
     }
